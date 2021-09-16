400c0a2e1b265e36b9d99f504b95b57044d33ada
rh-galaxy-droid <ansible-hub-ui+cloud@example.com>
2021-09-16 09:09:37 +0000

    Rework of collection version select (#870)
    * rework collection version select
    
    * fix translations
    
    * add pagination and limit version list
    
    * fix merge conflict
    
    * remove onClose function

--- components/headers/collection-header.js
+++ components/headers/collection-header.js
@@ -29,22 +29,44 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
 import { t, Trans } from '@lingui/macro';
 import * as React from 'react';
 import './header.scss';
+import * as moment from 'moment';
 import { ExternalLinkAltIcon } from '@patternfly/react-icons';
-import { FormSelect, FormSelectOption, Alert } from '@patternfly/react-core';
+import { Select, SelectOption, SelectVariant, List, ListItem, Modal, Alert, Text, Button, } from '@patternfly/react-core';
 import { AppContext } from 'src/loaders/app-context';
-import { BaseHeader, Breadcrumbs, LinkTabs, RepoSelector, } from 'src/components';
+import { BaseHeader, Breadcrumbs, LinkTabs, RepoSelector, Pagination, } from 'src/components';
 import { Paths, formatPath } from 'src/paths';
 import { ParamHelper } from 'src/utilities/param-helper';
 import { DateComponent } from '../date-component/date-component';
+import { Constants } from 'src/constants';
 var CollectionHeader = /** @class */ (function (_super) {
     __extends(CollectionHeader, _super);
-    function CollectionHeader() {
-        var _this = _super !== null && _super.apply(this, arguments) || this;
-        _this.ignoreParams = ['showing', 'keyords'];
+    function CollectionHeader(props) {
+        var _this = _super.call(this, props) || this;
+        _this.ignoreParams = ['showing', 'keywords'];
+        _this.updatePaginationParams = function (_a) {
+            var page = _a.page, page_size = _a.page_size;
+            _this.setState({
+                modalPagination: {
+                    page: page,
+                    pageSize: page_size,
+                },
+            });
+        };
+        _this.state = {
+            isOpenVersionsSelect: false,
+            isOpenVersionsModal: false,
+            modalPagination: {
+                page: 1,
+                pageSize: Constants.DEFAULT_PAGINATION_OPTIONS[1],
+            },
+        };
         return _this;
     }
     CollectionHeader.prototype.render = function () {
+        var _this = this;
         var _a = this.props, collection = _a.collection, params = _a.params, updateParams = _a.updateParams, breadcrumbs = _a.breadcrumbs, activeTab = _a.activeTab, className = _a.className;
+        var _b = this.state, modalPagination = _b.modalPagination, isOpenVersionsModal = _b.isOpenVersionsModal, isOpenVersionsSelect = _b.isOpenVersionsSelect;
+        var numOfshownVersions = 10;
         var all_versions = __spreadArray([], collection.all_versions, true);
         var match = all_versions.find(function (x) { return x.version === collection.latest_version.version; });
         if (!match) {
@@ -61,30 +83,73 @@ var CollectionHeader = /** @class */ (function (_super) {
             { key: 'repository', name: t(templateObject_4 || (templateObject_4 = __makeTemplateObject(["Repo"], ["Repo"]))) },
         ];
         var latestVersion = collection.latest_version.created_at;
-        return (React.createElement(BaseHeader, { className: className, title: collection.name, imageURL: collection.namespace.avatar_url, contextSelector: React.createElement(RepoSelector, { selectedRepo: this.context.selectedRepo, path: Paths.searchByRepo, isDisabled: true }), breadcrumbs: React.createElement(Breadcrumbs, { links: breadcrumbs }), versionControl: React.createElement("div", { className: 'install-version-column' },
-                React.createElement("span", null, t(templateObject_5 || (templateObject_5 = __makeTemplateObject(["Version"], ["Version"])))),
-                React.createElement("div", { className: 'install-version-dropdown' },
-                    React.createElement(FormSelect, { onChange: function (val) {
-                            return updateParams(ParamHelper.setParam(params, 'version', val));
-                        }, value: collection.latest_version.version, "aria-label": t(templateObject_6 || (templateObject_6 = __makeTemplateObject(["Select collection version"], ["Select collection version"]))) }, all_versions.map(function (v) { return (React.createElement(FormSelectOption, { key: v.version, value: v.version, label: 'v' + v.version })); }))),
-                latestVersion ? (React.createElement("span", { className: 'last-updated' },
-                    React.createElement(Trans, null,
-                        "Last updated ",
-                        React.createElement(DateComponent, { date: latestVersion })))) : null) },
-            collection.deprecated && (React.createElement(Alert, { variant: 'danger', isInline: true, title: t(templateObject_7 || (templateObject_7 = __makeTemplateObject(["This collection has been deprecated."], ["This collection has been deprecated."]))) })),
-            React.createElement("div", { className: 'tab-link-container' },
-                React.createElement("div", { className: 'tabs' }, this.renderTabs(activeTab)),
-                React.createElement("div", { className: 'links' },
-                    React.createElement("div", null,
-                        React.createElement(ExternalLinkAltIcon, null)),
-                    urlKeys.map(function (link) {
-                        var l = collection.latest_version.metadata[link.key];
-                        if (!l) {
-                            return null;
-                        }
-                        return (React.createElement("div", { className: 'link', key: link.key },
-                            React.createElement("a", { href: l, target: '_blank' }, link.name)));
-                    })))));
+        var isLatestVersion = function (v) {
+            return moment(v.created).fromNow() + " " + (v.version === all_versions[0].version ? t(templateObject_5 || (templateObject_5 = __makeTemplateObject(["(latest)"], ["(latest)"]))) : '');
+        };
+        var collectionName = collection.name;
+        return (React.createElement(React.Fragment, null,
+            React.createElement(Modal, { isOpen: isOpenVersionsModal, title: t(templateObject_6 || (templateObject_6 = __makeTemplateObject(["Collection versions"], ["Collection versions"]))), variant: 'small', onClose: function () { return _this.setState({ isOpenVersionsModal: false }); } },
+                React.createElement(List, { isPlain: true },
+                    React.createElement("div", { className: 'versions-modal-header' },
+                        React.createElement(Text, null, t(templateObject_7 || (templateObject_7 = __makeTemplateObject(["", "'s versions."], ["", "'s versions."])), collectionName)),
+                        React.createElement(Pagination, { isTop: true, params: {
+                                page: modalPagination.page,
+                                page_size: modalPagination.pageSize,
+                            }, updateParams: this.updatePaginationParams, count: all_versions.length })),
+                    this.paginateVersions(all_versions).map(function (v, i) { return (React.createElement(ListItem, { key: i },
+                        React.createElement(Button, { variant: 'link', isInline: true, onClick: function () {
+                                updateParams(ParamHelper.setParam(params, 'version', v.version.toString()));
+                                _this.setState({ isOpenVersionsModal: false });
+                            } },
+                            "v",
+                            v.version),
+                        ' ', t(templateObject_8 || (templateObject_8 = __makeTemplateObject(["released ", ""], ["released ", ""])), isLatestVersion(v)))); })),
+                React.createElement(Pagination, { params: {
+                        page: modalPagination.page,
+                        page_size: modalPagination.pageSize,
+                    }, updateParams: this.updatePaginationParams, count: all_versions.length })),
+            React.createElement(BaseHeader, { className: className, title: collection.name, imageURL: collection.namespace.avatar_url, contextSelector: React.createElement(RepoSelector, { selectedRepo: this.context.selectedRepo, path: Paths.searchByRepo, isDisabled: true }), breadcrumbs: React.createElement(Breadcrumbs, { links: breadcrumbs }), versionControl: React.createElement("div", { className: 'install-version-column' },
+                    React.createElement("span", null, t(templateObject_9 || (templateObject_9 = __makeTemplateObject(["Version"], ["Version"])))),
+                    React.createElement("div", { className: 'install-version-dropdown' },
+                        React.createElement(Select, { isOpen: isOpenVersionsSelect, onToggle: function (isOpenVersionsSelect) {
+                                return _this.setState({ isOpenVersionsSelect: isOpenVersionsSelect });
+                            }, variant: SelectVariant.single, onSelect: function () {
+                                return _this.setState({ isOpenVersionsSelect: false });
+                            }, selections: "v" + collection.latest_version.version, "aria-label": t(templateObject_10 || (templateObject_10 = __makeTemplateObject(["Select collection version"], ["Select collection version"]))), loadingVariant: numOfshownVersions < all_versions.length
+                                ? {
+                                    text: t(templateObject_11 || (templateObject_11 = __makeTemplateObject(["View more"], ["View more"]))),
+                                    onClick: function () {
+                                        return _this.setState({
+                                            isOpenVersionsModal: true,
+                                            isOpenVersionsSelect: false,
+                                        });
+                                    },
+                                }
+                                : null }, this.renderSelectVersions(all_versions, numOfshownVersions).map(function (v) { return (React.createElement(SelectOption, { key: v.version, value: "v" + v.version, onClick: function () {
+                                return updateParams(ParamHelper.setParam(params, 'version', v.version.toString()));
+                            } },
+                            React.createElement(Trans, null,
+                                v.version,
+                                " released ",
+                                isLatestVersion(v)))); }))),
+                    latestVersion ? (React.createElement("span", { className: 'last-updated' },
+                        React.createElement(Trans, null,
+                            "Last updated ",
+                            React.createElement(DateComponent, { date: latestVersion })))) : null) },
+                collection.deprecated && (React.createElement(Alert, { variant: 'danger', isInline: true, title: t(templateObject_12 || (templateObject_12 = __makeTemplateObject(["This collection has been deprecated."], ["This collection has been deprecated."]))) })),
+                React.createElement("div", { className: 'tab-link-container' },
+                    React.createElement("div", { className: 'tabs' }, this.renderTabs(activeTab)),
+                    React.createElement("div", { className: 'links' },
+                        React.createElement("div", null,
+                            React.createElement(ExternalLinkAltIcon, null)),
+                        urlKeys.map(function (link) {
+                            var url = collection.latest_version.metadata[link.key];
+                            if (!url) {
+                                return null;
+                            }
+                            return (React.createElement("div", { className: 'link', key: link.key },
+                                React.createElement("a", { href: url, target: '_blank' }, link.name)));
+                        }))))));
     };
     CollectionHeader.prototype.renderTabs = function (active) {
         var _a = this.props, params = _a.params, repo = _a.repo;
@@ -97,30 +162,37 @@ var CollectionHeader = /** @class */ (function (_super) {
         var tabs = [
             {
                 active: active === 'install',
-                title: t(templateObject_8 || (templateObject_8 = __makeTemplateObject(["Install"], ["Install"]))),
+                title: t(templateObject_13 || (templateObject_13 = __makeTemplateObject(["Install"], ["Install"]))),
                 link: formatPath(Paths.collectionByRepo, pathParams, reduced),
             },
             {
                 active: active === 'documentation',
-                title: t(templateObject_9 || (templateObject_9 = __makeTemplateObject(["Documentation"], ["Documentation"]))),
+                title: t(templateObject_14 || (templateObject_14 = __makeTemplateObject(["Documentation"], ["Documentation"]))),
                 link: formatPath(Paths.collectionDocsIndexByRepo, pathParams, reduced),
             },
             {
                 active: active === 'contents',
-                title: t(templateObject_10 || (templateObject_10 = __makeTemplateObject(["Contents"], ["Contents"]))),
+                title: t(templateObject_15 || (templateObject_15 = __makeTemplateObject(["Contents"], ["Contents"]))),
                 link: formatPath(Paths.collectionContentListByRepo, pathParams, reduced),
             },
             {
                 active: active === 'import-log',
-                title: t(templateObject_11 || (templateObject_11 = __makeTemplateObject(["Import log"], ["Import log"]))),
+                title: t(templateObject_16 || (templateObject_16 = __makeTemplateObject(["Import log"], ["Import log"]))),
                 link: formatPath(Paths.collectionImportLogByRepo, pathParams, reduced),
             },
         ];
         return React.createElement(LinkTabs, { tabs: tabs });
     };
+    CollectionHeader.prototype.renderSelectVersions = function (versions, count) {
+        return versions.slice(0, count);
+    };
+    CollectionHeader.prototype.paginateVersions = function (versions) {
+        var modalPagination = this.state.modalPagination;
+        return versions.slice(modalPagination.pageSize * (modalPagination.page - 1), modalPagination.pageSize * modalPagination.page);
+    };
     CollectionHeader.contextType = AppContext;
     return CollectionHeader;
 }(React.Component));
 export { CollectionHeader };
-var templateObject_1, templateObject_2, templateObject_3, templateObject_4, templateObject_5, templateObject_6, templateObject_7, templateObject_8, templateObject_9, templateObject_10, templateObject_11;
+var templateObject_1, templateObject_2, templateObject_3, templateObject_4, templateObject_5, templateObject_6, templateObject_7, templateObject_8, templateObject_9, templateObject_10, templateObject_11, templateObject_12, templateObject_13, templateObject_14, templateObject_15, templateObject_16;
 //# sourceMappingURL=collection-header.js.map
