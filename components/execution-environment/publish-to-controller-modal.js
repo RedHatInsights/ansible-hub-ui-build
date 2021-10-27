9ca7f682af0e7ee52d905a846a6a44a98e8b29ce
rh-galaxy-droid <ansible-hub-ui+cloud@example.com>
2021-10-27 14:55:34 +0000

    Add warning and ClipboardCopy to links (#1135)
    * Add warning aand ClipboardCopy to links
    
    Fixes https://issues.redhat.com/browse/AAH-1021
    
    * Rework ClipboardCopy to ClipboardCopyButton
    
    * Fix links according to UX feedback
    
    * Fix tooltip

--- components/execution-environment/publish-to-controller-modal.js
+++ components/execution-environment/publish-to-controller-modal.js
@@ -39,7 +39,7 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
 };
 import * as React from 'react';
 import { t, Trans } from '@lingui/macro';
-import { Button, DescriptionList, DescriptionListDescription, DescriptionListGroup, DescriptionListTerm, Flex, FlexItem, List, ListItem, Modal, } from '@patternfly/react-core';
+import { Button, ClipboardCopyButton, DescriptionList, DescriptionListDescription, DescriptionListGroup, DescriptionListTerm, Flex, FlexItem, List, ListItem, Modal, } from '@patternfly/react-core';
 import { ExternalLinkAltIcon, TagIcon } from '@patternfly/react-icons';
 import { ControllerAPI, ExecutionEnvironmentAPI } from 'src/api';
 import { APISearchTypeAhead, AlertList, AppliedFilters, CompoundFilter, EmptyStateFilter, EmptyStateNoData, LoadingPageSpinner, Pagination, ShaLabel, closeAlertMixin, } from 'src/components';
@@ -163,6 +163,7 @@ var PublishToControllerModal = /** @class */ (function (_super) {
         var _a = this.props, image = _a.image, isOpen = _a.isOpen;
         var _b = this.state, controllers = _b.controllers, controllerCount = _b.controllerCount, digest = _b.digest, tag = _b.tag;
         var url = getContainersURL();
+        var unsafeLinksSupported = !Object.keys(window).includes('chrome');
         if (!isOpen || !controllers) {
             return null;
         }
@@ -179,8 +180,9 @@ var PublishToControllerModal = /** @class */ (function (_super) {
             return (React.createElement(ListItem, { style: { paddingTop: '8px' } },
                 React.createElement("a", { href: href, target: '_blank' }, host),
                 ' ',
-                React.createElement("small", null,
-                    React.createElement(ExternalLinkAltIcon, null))));
+                unsafeLinksSupported && (React.createElement("small", null,
+                    React.createElement(ExternalLinkAltIcon, null))),
+                !unsafeLinksSupported && (React.createElement(ClipboardCopyButton, { variant: 'plain', children: t(templateObject_4 || (templateObject_4 = __makeTemplateObject(["Copy to clipboard"], ["Copy to clipboard"]))), id: href, textId: t(templateObject_5 || (templateObject_5 = __makeTemplateObject(["Copy to clipboard"], ["Copy to clipboard"]))), onClick: function () { return navigator.clipboard.writeText(href); } }))));
         })));
     };
     PublishToControllerModal.prototype.render = function () {
@@ -190,27 +192,28 @@ var PublishToControllerModal = /** @class */ (function (_super) {
         var docsLink = 'https://access.redhat.com/documentation/en-us/red_hat_ansible_automation_platform/2.1';
         var noData = (controllers === null || controllers === void 0 ? void 0 : controllers.length) === 0 &&
             !filterIsSet(controllerParams, ['host__icontains']);
-        var notListedMessage = (React.createElement(React.Fragment, null, t(templateObject_4 || (templateObject_4 = __makeTemplateObject(["If the Controller is not listed in the table, check settings.py."], ["If the Controller is not listed in the table, check settings.py."]))),
+        var notListedMessage = (React.createElement(React.Fragment, null, t(templateObject_6 || (templateObject_6 = __makeTemplateObject(["If the Controller is not listed in the table, check settings.py."], ["If the Controller is not listed in the table, check settings.py."]))),
             ' ',
             docsLink && (React.createElement(React.Fragment, null,
-                React.createElement("a", { href: docsLink, target: '_blank' }, t(templateObject_5 || (templateObject_5 = __makeTemplateObject(["Learn more"], ["Learn more"])))),
+                React.createElement("a", { href: docsLink, target: '_blank' }, t(templateObject_7 || (templateObject_7 = __makeTemplateObject(["Learn more"], ["Learn more"])))),
                 ' ',
                 React.createElement(ExternalLinkAltIcon, null)))));
         var Spacer = function () { return React.createElement("div", { style: { paddingTop: '24px' } }); };
-        return (React.createElement(Modal, { variant: 'large', title: t(templateObject_6 || (templateObject_6 = __makeTemplateObject(["Use in Controller"], ["Use in Controller"]))), isOpen: isOpen, onClose: onClose, actions: [
-                React.createElement(Button, { key: 'close', variant: 'primary', onClick: onClose }, t(templateObject_7 || (templateObject_7 = __makeTemplateObject(["Close"], ["Close"])))),
+        var unsafeLinksSupported = !Object.keys(window).includes('chrome');
+        return (React.createElement(Modal, { variant: 'large', title: t(templateObject_8 || (templateObject_8 = __makeTemplateObject(["Use in Controller"], ["Use in Controller"]))), isOpen: isOpen, onClose: onClose, actions: [
+                React.createElement(Button, { key: 'close', variant: 'primary', onClick: onClose }, t(templateObject_9 || (templateObject_9 = __makeTemplateObject(["Close"], ["Close"])))),
             ] },
             React.createElement(AlertList, { alerts: alerts, closeAlert: function (i) { return _this.closeAlert(i); } }),
             loading && (React.createElement("div", { style: { padding: '16px' } },
                 React.createElement(LoadingPageSpinner, null))),
-            noData && !loading ? (React.createElement(EmptyStateNoData, { title: t(templateObject_8 || (templateObject_8 = __makeTemplateObject(["No Controllers available"], ["No Controllers available"]))), description: notListedMessage })) : null,
+            noData && !loading ? (React.createElement(EmptyStateNoData, { title: t(templateObject_10 || (templateObject_10 = __makeTemplateObject(["No Controllers available"], ["No Controllers available"]))), description: notListedMessage })) : null,
             isOpen && !loading && !noData && controllers && (React.createElement(React.Fragment, null,
                 React.createElement(DescriptionList, { isHorizontal: true },
                     React.createElement(DescriptionListGroup, null,
-                        React.createElement(DescriptionListTerm, null, t(templateObject_9 || (templateObject_9 = __makeTemplateObject(["Execution Environment"], ["Execution Environment"])))),
+                        React.createElement(DescriptionListTerm, null, t(templateObject_11 || (templateObject_11 = __makeTemplateObject(["Execution Environment"], ["Execution Environment"])))),
                         React.createElement(DescriptionListDescription, null, image)),
                     React.createElement(DescriptionListGroup, null,
-                        React.createElement(DescriptionListTerm, null, t(templateObject_10 || (templateObject_10 = __makeTemplateObject(["Tag"], ["Tag"])))),
+                        React.createElement(DescriptionListTerm, null, t(templateObject_12 || (templateObject_12 = __makeTemplateObject(["Tag"], ["Tag"])))),
                         React.createElement(DescriptionListDescription, null,
                             React.createElement(Flex, null,
                                 React.createElement(FlexItem, null,
@@ -223,15 +226,19 @@ var PublishToControllerModal = /** @class */ (function (_super) {
                                                 tagSelection: [{ id: value, name: value }],
                                                 digest: digest,
                                             });
-                                        }, placeholderText: t(templateObject_11 || (templateObject_11 = __makeTemplateObject(["Select a tag"], ["Select a tag"]))), results: tagResults, selections: tagSelection, toggleIcon: React.createElement(TagIcon, null) })),
+                                        }, placeholderText: t(templateObject_13 || (templateObject_13 = __makeTemplateObject(["Select a tag"], ["Select a tag"]))), results: tagResults, selections: tagSelection, toggleIcon: React.createElement(TagIcon, null) })),
                                 React.createElement(FlexItem, null)))),
                     digest && (React.createElement(React.Fragment, null,
                         React.createElement(DescriptionListGroup, null,
-                            React.createElement(DescriptionListTerm, null, t(templateObject_12 || (templateObject_12 = __makeTemplateObject(["Digest"], ["Digest"])))),
+                            React.createElement(DescriptionListTerm, null, t(templateObject_14 || (templateObject_14 = __makeTemplateObject(["Digest"], ["Digest"])))),
                             React.createElement(DescriptionListDescription, null,
                                 React.createElement(ShaLabel, { grey: true, long: true, digest: digest })))))),
                 React.createElement(Spacer, null),
                 React.createElement(Trans, null, "Click on the Controller URL that you want to use the above execution environment in, and it will launch that Controller's console. Log in (if necessary) and follow the steps to complete the configuration."),
+                React.createElement("br", null),
+                !unsafeLinksSupported && (React.createElement(Trans, null,
+                    React.createElement("b", null, "Note:"),
+                    " The following links may be blocked by your browser. Copy and paste the link manually.")),
                 React.createElement(Spacer, null),
                 React.createElement(Flex, null,
                     React.createElement(FlexItem, null,
@@ -243,7 +250,7 @@ var PublishToControllerModal = /** @class */ (function (_super) {
                             }, params: controllerParams, filterConfig: [
                                 {
                                     id: 'host__icontains',
-                                    title: t(templateObject_13 || (templateObject_13 = __makeTemplateObject(["Controller name"], ["Controller name"]))),
+                                    title: t(templateObject_15 || (templateObject_15 = __makeTemplateObject(["Controller name"], ["Controller name"]))),
                                 },
                             ] })),
                     React.createElement(FlexItem, { grow: { default: 'grow' } }),
@@ -258,7 +265,7 @@ var PublishToControllerModal = /** @class */ (function (_super) {
                             return _this.fetchControllers();
                         });
                     }, params: controllerParams, ignoredParams: ['page_size', 'page'], niceNames: {
-                        host__icontains: t(templateObject_14 || (templateObject_14 = __makeTemplateObject(["Controller name"], ["Controller name"]))),
+                        host__icontains: t(templateObject_16 || (templateObject_16 = __makeTemplateObject(["Controller name"], ["Controller name"]))),
                     } }),
                 React.createElement(Spacer, null),
                 this.renderControllers(),
@@ -281,5 +288,5 @@ var PublishToControllerModal = /** @class */ (function (_super) {
     return PublishToControllerModal;
 }(React.Component));
 export { PublishToControllerModal };
-var templateObject_1, templateObject_2, templateObject_3, templateObject_4, templateObject_5, templateObject_6, templateObject_7, templateObject_8, templateObject_9, templateObject_10, templateObject_11, templateObject_12, templateObject_13, templateObject_14;
+var templateObject_1, templateObject_2, templateObject_3, templateObject_4, templateObject_5, templateObject_6, templateObject_7, templateObject_8, templateObject_9, templateObject_10, templateObject_11, templateObject_12, templateObject_13, templateObject_14, templateObject_15, templateObject_16;
 //# sourceMappingURL=publish-to-controller-modal.js.map
