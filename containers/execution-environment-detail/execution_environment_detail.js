63f55217079c3cd2028ebdfecb0dec73b168ed4c
rh-galaxy-droid <ansible-hub-ui+cloud@example.com>
2021-11-03 21:29:01 +0000

    Cypress test for execution environments (#1138)
    * testing execution exvironments
    
    * testing execution environments
    
    * remove registries/containers before testing

--- containers/execution-environment-detail/execution_environment_detail.js
+++ containers/execution-environment-detail/execution_environment_detail.js
@@ -70,15 +70,17 @@ var ExecutionEnvironmentDetail = /** @class */ (function (_super) {
                     React.createElement(Title, { headingLevel: 'h2', size: 'lg' }, !this.state.markdownEditing && this.state.readme && canEdit && (React.createElement(Button, { className: 'edit-button', variant: 'primary', onClick: function () {
                             _this.setState({ markdownEditing: true });
                         } }, t(templateObject_3 || (templateObject_3 = __makeTemplateObject(["Edit"], ["Edit"])))))),
-                    !this.state.markdownEditing && !this.state.readme ? (React.createElement(EmptyStateNoData, { title: t(templateObject_4 || (templateObject_4 = __makeTemplateObject(["No README"], ["No README"]))), description: t(templateObject_5 || (templateObject_5 = __makeTemplateObject(["Add a README with instructions for using this container."], ["Add a README with instructions for using this container."]))), button: canEdit ? (React.createElement(Button, { variant: 'primary', onClick: function () { return _this.setState({ markdownEditing: true }); } }, t(templateObject_6 || (templateObject_6 = __makeTemplateObject(["Add"], ["Add"]))))) : null })) : (React.createElement(MarkdownEditor, { text: this.state.readme, placeholder: '', helperText: '', updateText: function (value) {
+                    !this.state.markdownEditing && !this.state.readme ? (React.createElement(EmptyStateNoData, { title: t(templateObject_4 || (templateObject_4 = __makeTemplateObject(["No README"], ["No README"]))), description: t(templateObject_5 || (templateObject_5 = __makeTemplateObject(["Add a README with instructions for using this container."], ["Add a README with instructions for using this container."]))), button: canEdit ? (React.createElement("div", { "data-cy": 'add-readme' },
+                            React.createElement(Button, { variant: 'primary', onClick: function () { return _this.setState({ markdownEditing: true }); } }, t(templateObject_6 || (templateObject_6 = __makeTemplateObject(["Add"], ["Add"])))))) : null })) : (React.createElement(MarkdownEditor, { text: this.state.readme, placeholder: '', helperText: '', updateText: function (value) {
                             return _this.setState({
                                 readme: value,
                             });
                         }, editing: this.state.markdownEditing })),
                     this.state.markdownEditing && (React.createElement(React.Fragment, null,
-                        React.createElement(Button, { variant: 'primary', onClick: function () {
-                                return _this.saveReadme(_this.props.containerRepository.name, _this.state.readme);
-                            } }, t(templateObject_7 || (templateObject_7 = __makeTemplateObject(["Save"], ["Save"])))),
+                        React.createElement("div", { "data-cy": 'save-readme' },
+                            React.createElement(Button, { variant: 'primary', onClick: function () {
+                                    return _this.saveReadme(_this.props.containerRepository.name, _this.state.readme);
+                                } }, t(templateObject_7 || (templateObject_7 = __makeTemplateObject(["Save"], ["Save"]))))),
                         React.createElement(Button, { variant: 'link', onClick: function () {
                                 _this.setState({
                                     markdownEditing: false,
