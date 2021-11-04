d49caa0a4b0bf1bf8b48cdccf9fb1da05fa31a67
rh-galaxy-droid <ansible-hub-ui+cloud@example.com>
2021-11-04 14:42:07 +0000

    insights-loader: rework navigation logic to work both in prod-stable and prod-beta (#708)
    (notes on branch differences in #707)

--- api/response-types/permissions.js
+++
@@ -1,7 +0,0 @@
-var GroupObjectPermissionType = /** @class */ (function () {
-    function GroupObjectPermissionType() {
-    }
-    return GroupObjectPermissionType;
-}());
-export { GroupObjectPermissionType };
-//# sourceMappingURL=permissions.js.map
