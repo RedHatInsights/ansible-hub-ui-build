d28300091eabc4d21aade3624b3456cc9e39bbfe
rh-galaxy-droid <ansible-hub-ui+cloud@example.com>
2021-12-07 14:23:28 +0000

    eslint config with typescript support, some basic rules (#860)
    * config/test.webpack.config.js: has been using an undefined TSOverrides variable since 2019, not used :)
    
    * npm install --save-dev @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-prettier eslint-plugin-react typescript
    
    * drop old unused eslint config
    
    * package.json: add tasks eslint, eslint:fix
    
    * eslint - enable for js and ts, minimal config
    
    the config is really just the defaults, globals and plugins,
    and then disabling any rules that generates warnings for now
    
    so that we can enable eslint, and then fix individual rules in separate PRs
    
    * fix a couple of eslint issues
    
        /home/himdel/ansible-hub-ui/config/webpack.base.config.js
          1:11  error  'require' is already defined as a built-in global variable    no-redeclare
          1:20  error  'module' is already defined as a built-in global variable     no-redeclare
          1:28  error  '__dirname' is already defined as a built-in global variable  no-redeclare
    
        /home/himdel/ansible-hub-ui/src/components/collection-list/collection-filter.tsx
          49:34  error  Expected a `const` instead of a literal type assertion  @typescript-eslint/prefer-as-const
    
        /home/himdel/ansible-hub-ui/src/components/empty-state/empty-state-unauthorized.tsx
          7:11  error  An empty interface is equivalent to `{}`  @typescript-eslint/no-empty-interface
    
        /home/himdel/ansible-hub-ui/src/components/repositories/remote-repository-table.tsx
          46:14  error  Empty block statement  no-empty
    
        /home/himdel/ansible-hub-ui/src/containers/execution-environment-detail/execution_environment_detail_activities.tsx
          72:39  error  Unexpected empty arrow function  @typescript-eslint/no-empty-function
    
        /home/himdel/ansible-hub-ui/src/index.d.ts
          11:1  error  Unexpected var, use let or const instead  no-var
          12:1  error  Unexpected var, use let or const instead  no-var
          13:1  error  Unexpected var, use let or const instead  no-var
          14:1  error  Unexpected var, use let or const instead  no-var
          15:1  error  Unexpected var, use let or const instead  no-var
          16:1  error  Unexpected var, use let or const instead  no-var
          17:1  error  Unexpected var, use let or const instead  no-var
    
        /home/himdel/ansible-hub-ui/src/loaders/insights/insights-loader.js
          26:5   error  'insights' is not defined  no-undef
          27:5   error  'insights' is not defined  no-undef
          32:19  error  'insights' is not defined  no-undef
          60:5   error  'insights' is not defined  no-undef
    
    * lint:js, lint:js:fix - run both eslint and prettier

--- loaders/insights/insights-loader.js
+++ loaders/insights/insights-loader.js
@@ -13,6 +13,7 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+/* global insights */
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { withRouter, matchPath } from 'react-router-dom';
