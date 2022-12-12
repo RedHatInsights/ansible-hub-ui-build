cb3e0bfb070be8371ca03bd9e139efd2e97b5698
rh-galaxy-droid <ansible-hub-ui+cloud@example.com>
2022-12-12 06:43:22 +0000

    React - switch to React 17, enable StrictMode (#2925)
    * React - switch to react 17
    
    No-Issue
    
    * React - enable StrictMode
    
    https://reactjs.org/docs/strict-mode.html
    
    * Fix type error - ReactNodeArray doesn't match any[]
    
        src/components/patternfly-wrappers/stateful-dropdown.tsx:63:9 - error TS2322: Type 'ReactNodeArray' is not assignable to type 'any[]'.
    
        63         dropdownItems={items}
                  ~~~~~~~~~~~~~
    
          node_modules/@patternfly/react-core/dist/esm/components/Dropdown/Dropdown.d.ts:11:5
            11     dropdownItems?: any[];
                  ~~~~~~~~~~~~~
            The expected type comes from property 'dropdownItems' which is declared here on type 'IntrinsicAttributes & DropdownProps & { children?: ReactNode; }'
    
        Found 1 error in src/components/patternfly-wrappers/stateful-dropdown.tsx:63
    
    * Router: fix Suspense fallback invocation

--- entry-standalone.js
+++ entry-standalone.js
@@ -5,9 +5,9 @@ import { I18nProvider } from '@lingui/react';
 import { i18n } from '@lingui/core';
 import App from './loaders/standalone/standalone-loader';
 import 'src/l10n';
-// Entrypoint for compiling the app to run in standalone mode (for all deployments
-// other than on the insights/cloud services environment)
-ReactDOM.render(React.createElement(Router, { basename: UI_BASE_PATH },
-    React.createElement(I18nProvider, { i18n: i18n },
-        React.createElement(App, null))), document.getElementById('root'));
+// Entrypoint for compiling the app to run in standalone mode
+ReactDOM.render(React.createElement(React.StrictMode, null,
+    React.createElement(Router, { basename: UI_BASE_PATH },
+        React.createElement(I18nProvider, { i18n: i18n },
+            React.createElement(App, null)))), document.getElementById('root'));
 //# sourceMappingURL=entry-standalone.js.map
