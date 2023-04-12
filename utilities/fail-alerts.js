6a9d580aea1536540ba439babd8a11f7a2b85868
rh-galaxy-droid <ansible-hub-ui+cloud@example.com>
2023-04-12 14:03:06 +0000

    Make alerts return error messages from API (#3542)
    No-Issue

--- utilities/fail-alerts.js
+++ utilities/fail-alerts.js
@@ -3,7 +3,8 @@ var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cook
     return cooked;
 };
 import { t } from '@lingui/macro';
-export function errorMessage(statusCode, statusText) {
+import { mapErrorMessages } from 'src/utilities';
+export function errorMessage(statusCode, statusText, customMessage) {
     var messages = {
         500: t(templateObject_1 || (templateObject_1 = __makeTemplateObject(["Error ", " - ", ": The server encountered an error and was unable to complete your request."], ["Error ", " - ", ": The server encountered an error and was unable to complete your request."])), statusCode, statusText),
         401: t(templateObject_2 || (templateObject_2 = __makeTemplateObject(["Error ", " - ", ": You do not have the required permissions to proceed with this request. Please contact the server administrator for elevated permissions."], ["Error ", " - ", ": You do not have the required permissions to proceed with this request. Please contact the server administrator for elevated permissions."])), statusCode, statusText),
@@ -11,17 +12,34 @@ export function errorMessage(statusCode, statusText) {
         404: t(templateObject_4 || (templateObject_4 = __makeTemplateObject(["Error ", " - ", ": The server could not find the requested URL."], ["Error ", " - ", ": The server could not find the requested URL."])), statusCode, statusText),
         400: t(templateObject_5 || (templateObject_5 = __makeTemplateObject(["Error ", " - ", ": The server was unable to complete your request."], ["Error ", " - ", ": The server was unable to complete your request."])), statusCode, statusText),
         default: t(templateObject_6 || (templateObject_6 = __makeTemplateObject(["Error ", " - ", ""], ["Error ", " - ", ""])), statusCode, statusText),
+        custom: t(templateObject_7 || (templateObject_7 = __makeTemplateObject(["Error ", " - ", ": ", ""], ["Error ", " - ", ": ", ""])), statusCode, statusText, customMessage),
     };
+    if (customMessage) {
+        return messages.custom;
+    }
     return messages[statusCode] || messages.default;
 }
 export var handleHttpError = function (title, callback, addAlert) { return function (e) {
     var _a = e.response, status = _a.status, statusText = _a.statusText;
+    console.log(typeof e.response.data);
+    var message = '';
+    var err_detail = mapErrorMessages(e);
+    for (var msg in err_detail) {
+        message = message + err_detail[msg] + ' ';
+    }
+    var description;
+    if (message !== '') {
+        description = errorMessage(status, statusText, message);
+    }
+    else {
+        description = errorMessage(status, statusText);
+    }
     addAlert({
         title: title,
         variant: 'danger',
-        description: errorMessage(status, statusText),
+        description: description,
     });
     callback();
 }; };
-var templateObject_1, templateObject_2, templateObject_3, templateObject_4, templateObject_5, templateObject_6;
+var templateObject_1, templateObject_2, templateObject_3, templateObject_4, templateObject_5, templateObject_6, templateObject_7;
 //# sourceMappingURL=fail-alerts.js.map
